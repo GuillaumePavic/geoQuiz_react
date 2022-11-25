c0e62ad79718772b72e0d626baacae846485cd4d
GuillaumePavic <guillaume.pavic.pro@gmail.com>
2022-11-25 10:59:56 +0100

    Add components structure and router

---
+++ src/components/Leaderboard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Leaderboard: React.FC = () => {
+    return (
+        <div>Leaderboard</div>
+    )
+}
+
+export default Leaderboard;
---
+++ src/components/Quizz.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Quizz: React.FC = () => {
+    return(
+        <div>Quizz</div>
+    )
+}
+
+export default Quizz;
