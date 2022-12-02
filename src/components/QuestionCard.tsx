feeed8be38a1166fa994e6a877902c7e8a0685de
GuillaumePavic <guillaume.pavic.pro@gmail.com>
2022-12-02 16:06:42 +0100

    Add basic timer

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import Quiz_data from '../Interfaces/Quiz_data.interface';
 
 
@@ -51,6 +51,34 @@ const QuestionNumber = styled.div`
     font-size: 2rem;
 `;
 
+const ProgressBarContainer = styled.div`
+    width:90%;
+    margin: 0 auto;
+    margin-top: 20px;
+    height:10px;
+    border:1px silver solid;
+    border-radius:4px;
+    background:white;
+`;
+
+const ProgressBarAnimation = keyframes`
+    100% { transform: scaleX(0); background:red;}
+    50%  { background:orange;}
+    0%   { transform: scaleX(1); background:green;}
+`;
+
+const ProgressBar = styled.div`
+    height:100%;
+    text-align:right;
+    font:bold 12px arial;
+    border-right:1px silver solid;
+    border-top-right-radius:4px;
+    border-bottom-right-radius:4px;
+    line-height:30px;
+    color:#444;
+    animation: 10s linear 1 ${ProgressBarAnimation};	
+`;
+
 interface Props {
     currentQuestion: Quiz_data | undefined,
     totalQuestions: number | undefined,
@@ -77,9 +105,9 @@ const QuestionCard: React.FC<Props> = ({
 
     useEffect(()=>{
         const timer = setTimeout(() =>{
-            handleTimerSubmit(answerRef.current); 
+            // handleTimerSubmit(answerRef.current); 
             inputElement.current!.value = '';
-        }, 10000);
+        }, 1000);
 
         return () => clearTimeout(timer);
     }, [indexCurrentQuestion]);
@@ -96,9 +124,40 @@ const QuestionCard: React.FC<Props> = ({
                     onKeyDown={ (e) => {handlePlayerSubmit(e)}} 
                     autoFocus 
                 />
+            <ProgressBarContainer>
+                <ProgressBar></ProgressBar>
+            </ProgressBarContainer>
             </Question>
         </QuestionWrapper>
     )
 }
 
 export default QuestionCard;
+
+/* #progressContainer {
+	width:90%;
+	margin:0 auto;
+    margin-top: 20px;
+	height:10px;
+	border:1px silver solid;
+	border-radius:4px;
+	background:white;
+}
+
+#progress {
+	height:100%;
+	text-align:right;
+	font:bold 12px arial;
+	border-right:1px silver solid;
+	border-top-right-radius:4px;
+	border-bottom-right-radius:4px;
+	line-height:30px;
+	color:#444;
+  animation: 15s linear 1s 1   slidein;	
+}
+
+@keyframes slidein {
+    100% { transform: scaleX(0); background:red;}
+    50%  { background:orange;}
+    0%   { transform: scaleX(1); background:green;}
+  } */
